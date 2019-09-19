0c74e2692469af7bd695af283e49c2818660daf5
yeahjin0120 <55428346+yeahjin0120@users.noreply.github.com>
2019-09-19 15:35:54 +0900

    project2

--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello Sookmyung EE!¡¬n");
+	int x;
+	short y;
+	float z;
+	double a;
+	
+	printf("bytes of int : %d¡¬n" , sizeof(x));
+    printf("bytes of short : %d¡¬n" , sizeof(y));
+    printf("bytes of float : %d¡¬n" , sizeof(z));
+    printf("bytes of double : %d¡¬n" , sizeof(a));
 	
 	return 0;
 }
